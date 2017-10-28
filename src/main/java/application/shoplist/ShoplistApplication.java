3e6ca1ff96b30fddd3b876376fd300dbccbe89d0
Aleksandr <Salatiques@gmail.com>
2017-10-28 20:54:24 +0300

    Initial commit + hello world

---
+++ src/main/java/application/shoplist/ShoplistApplication.java
@@ -0,0 +1,13 @@
+package application.shoplist;
+
+import config.JpaConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoplistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(new Class<?>[] {ShoplistApplication.class, JpaConfig.class}, args);
+	}
+}
