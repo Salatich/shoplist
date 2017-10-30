a73af5d61cf5a2ec46d1f4e6679b25924b43166b
Olga <debxit@gmail.com>
2017-10-30 23:05:36 +0300

    created maven modules

--- backend/src/main/java/application/shoplist/ShoplistApplication.java
+++ backend/src/main/java/application/shoplist/ShoplistApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShoplistApplication {
+public class  ShoplistApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(new Class<?>[] {ShoplistApplication.class, JpaConfig.class}, args);
