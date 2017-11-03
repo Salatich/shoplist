e4a1f9b37163bbc4c3453118c9b9d08b1810de2a
Olga <debxit@gmail.com>
2017-11-03 11:26:20 +0300

    data service and additional dependency

---
+++ frontend/src/main/src/app/services/data.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {Http} from '@angular/http';
+import {Response, Headers} from '@angular/http';
+import {IList} from '../IList';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+
+
+@Injectable()
+export class DataService {
+ 
+  constructor(private http: Http){ }
+  
+  postData(obj: IList){
+    
+    const body = JSON.stringify(obj);
+    
+    let headers = new Headers({ 'Content-Type': 'application/json;charset=utf-8' });
+    
+    return this.http.post('http://localhost:8080/CreateList', body, { headers: headers })
+      .map((resp:Response)=>resp.json())
+      .catch((error:any) =>{return Observable.throw(error);});
+  }
+}
